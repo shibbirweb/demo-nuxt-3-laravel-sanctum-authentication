be96ca9d34e563e0b7b452f30b217b7d61209526
Md. Shibbir Ahmed <shibbirweb@gmail.com>
2023-08-06 13:22:45 +0600

    Init project

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'type' => 'Bearer',
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    $user = auth()->user();
+
+    // Revoke all tokens...
+    $user->tokens()->delete();
+
+    return response()->json([
+        'message' => 'success',
+    ]);
+});
